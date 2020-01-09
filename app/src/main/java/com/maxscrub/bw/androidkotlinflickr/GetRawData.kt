29fa4960b642679cfe05c3a6513f978994007a65
BW <bmwanz@live.com>
2020-01-08 17:28:03 -0800

    start async task

---
+++ app/src/main/java/com/maxscrub/bw/androidkotlinflickr/GetRawData.kt
@@ -0,0 +1,57 @@
+package com.maxscrub.bw.androidkotlinflickr
+
+import android.os.AsyncTask
+import timber.log.Timber
+import java.io.IOException
+import java.net.MalformedURLException
+import java.net.URL
+
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALIZED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+class GetRawData : AsyncTask<String, Void, String>() {
+
+    private var downloadStatus = DownloadStatus.IDLE
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+        Timber.d("GetRawData.onPostExecute \n${result}")
+    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALIZED
+            Timber.e("GetRawData.doInBackground NOT_INITIALIZED")
+            return "No URL specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            Timber.d("GetRawData.doInBackground STATUS OK")
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when(e) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALIZED
+                    "doInBackground: Invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackground: IOException Reading data ${e.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackground: Security Exception: Needs permission? ${e.message}"
+                }
+                else -> {
+                    downloadStatus = DownloadStatus.ERROR
+                    "Unknown error: ${e.message}"
+                }
+            }
+
+            Timber.e(errorMessage)
+            return errorMessage;
+        }
+    }
+}
