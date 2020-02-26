7f80c796796f6be6626945411e73dc587e557721
BW <bmwanz@live.com>
2020-02-25 19:00:24 -0800

    simple click logging

--- app/src/main/java/com/maxscrub/bw/androidkotlinflickr/model/RecyclerItemClickListener.kt
+++ app/src/main/java/com/maxscrub/bw/androidkotlinflickr/model/RecyclerItemClickListener.kt
@@ -1,7 +1,9 @@
 package com.maxscrub.bw.androidkotlinflickr.model
 
 import android.content.Context
+import android.view.GestureDetector
 import android.view.MotionEvent
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.maxscrub.bw.androidkotlinflickr.interfaces.OnRecyclerClickListener
 import timber.log.Timber
@@ -16,8 +18,27 @@ class RecyclerItemClickListener(
 
     private val TAG = "RecyclerItemClickListen"
 
+    private val gestureDetector = GestureDetectorCompat(context, object: GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Timber.d("%s.onSingleTapUp", TAG)
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            listener.onItemClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Timber.d("%s.onLongPress", TAG)
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            listener.onItemLongClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+        }
+
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-        Timber.d("%s.onInterCeptTouchEvent: $e", TAG)
-        return true
+        Timber.d("%s.onInterceptTouchEvent: $e", TAG)
+        val result = gestureDetector.onTouchEvent(e)
+        Timber.d("%s.onInterceptTouchEvent return: $result", TAG)
+        return result
     }
 }
